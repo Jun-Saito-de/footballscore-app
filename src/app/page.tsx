938fe03b6f0e6a415f67ee22f5bc721f71b71201
Apple <j.saito@design-engineering.co.jp>
2025-06-04 10:02:22 +0900

    ヘッダーコンポーネントを作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="wrapper topContainer">
-      <div className="iconimage"><img src="/images/brazilflag.png" alt="" /></div>
-      <h1 className="title">Calendario da Serie A</h1>
-      <button className="greenBtn">試合一覧</button>
-      <button className="greenBtn">トップに戻る</button>      
-    </div>
+    <main>
+      <div className="wrapper topContainer">
+        <div className="iconimage"><img src="/images/brazilflag.png" alt="" /></div>
+        <h1 className="title">Calendario da Serie A</h1>
+        <button className="greenBtn">試合一覧</button>
+        <button className="greenBtn">トップに戻る</button>
+      </div>
+    </main>
   );
 }
